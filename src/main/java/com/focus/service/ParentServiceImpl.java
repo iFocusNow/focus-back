ea555d1603be7d33d9f3386513abc47fe8880e71
Pedro Soto <u202020808@upc.edu.pe>
2023-05-30 13:58:46 -0500

    feat: Register-Parent
    Updated Logic with Encryption
    
    Updated POM.xlm to use secuirty

--- src/main/java/com/focus/service/ParentServiceImpl.java
+++ src/main/java/com/focus/service/ParentServiceImpl.java
@@ -3,20 +3,27 @@ package com.focus.service;
 import com.focus.model.Parent;
 import com.focus.repository.ParentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+
+
 import java.util.List;
+
 
 @Service
 public class ParentServiceImpl implements ParentService {
     @Autowired
     private ParentRepository repo;
+
     public List<Parent> getAllParents() {
         List<Parent> parents = repo.findAll();
         return parents;
     }
 
     public Parent registerParent(Parent parent){
+        String encodedPassword = BCrypt.hashpw(parent.getPassword(), BCrypt.gensalt());
+        parent.setPassword(encodedPassword);
         return repo.save(parent);
     }
 }
