eeb9788bc714b5cca7f16391001c306ba36907c0
Pedro Soto <u202020808@upc.edu.pe>
2023-06-05 10:47:42 -0500

    Update ParentController.java

--- src/main/java/com/focus/controller/ParentController.java
+++ src/main/java/com/focus/controller/ParentController.java
@@ -8,11 +8,7 @@ import com.focus.service.ParentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
