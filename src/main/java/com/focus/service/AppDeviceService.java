7042996cadc1781d38ee58c50168e352438ade07
guerreroHC <guerreroh2505@gmail.com>
2023-06-06 14:11:34 -0500

    add-app-device done

--- src/main/java/com/focus/service/AppDeviceService.java
+++ src/main/java/com/focus/service/AppDeviceService.java
@@ -1,6 +1,8 @@
 package com.focus.service;
 
 import com.focus.dto.AppDeviceDTO;
+import com.focus.model.App;
+import com.focus.model.AppDevice;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,4 +10,6 @@ import java.util.UUID;
 public interface AppDeviceService {
     public List<AppDeviceDTO> getAllAppDevices(UUID device_id);
     public boolean deleteAppDevice(UUID device_id, UUID app_id);
+
+    public boolean save(AppDevice appDevice);
 }
