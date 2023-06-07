a99e369ae6269506f2ed2bb79aa06b9330181dec
guerreroHC <guerreroh2505@gmail.com>
2023-06-06 20:43:31 -0500

    add-link done

--- src/main/java/com/focus/service/LinkService.java
+++ src/main/java/com/focus/service/LinkService.java
@@ -1,6 +1,7 @@
 package com.focus.service;
 
 import com.focus.dto.LinkBlockPeriodDTO;
+import com.focus.model.Link;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,4 +9,6 @@ import java.util.UUID;
 public interface LinkService {
     public List<LinkBlockPeriodDTO> getAllLinkBlockPeriods(UUID device_id);
     public boolean deleteLink(UUID id);
+
+    public boolean save (Link link);
 }
--- src/main/java/com/focus/service/LinkServiceImpl.java
+++ src/main/java/com/focus/service/LinkServiceImpl.java
@@ -2,7 +2,11 @@ package com.focus.service;
 
 import com.focus.dto.AppDeviceDTO;
 import com.focus.dto.LinkBlockPeriodDTO;
+import com.focus.model.BlockPeriod;
+import com.focus.model.Device;
 import com.focus.model.Link;
+import com.focus.repository.BlockPeriodRepository;
+import com.focus.repository.DeviceRepository;
 import com.focus.repository.LinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,13 @@ import java.util.UUID;
 public class LinkServiceImpl implements LinkService {
     @Autowired
     private LinkRepository repo;
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @Autowired
+    private BlockPeriodRepository blockPeriodRepository;
+
     public List<LinkBlockPeriodDTO> getAllLinkBlockPeriods(UUID device_id) {
         List<Link> links = repo.findAllByDevice(device_id);
         List<LinkBlockPeriodDTO> linkBlockPeriodDTOs = new ArrayList<>();
@@ -45,6 +56,18 @@ public class LinkServiceImpl implements LinkService {
         }
 
         repo.deleteById(id);
+        return true;
+    }
+
+    public boolean save(Link link) {
+        if(deviceRepository.findById(link.getDevice().getId()).isEmpty()) return false;
+        if(blockPeriodRepository.findById(link.getBlock_period().getId()).isEmpty()) return false;
+
+        Device deviceFound = deviceRepository.findById(link.getDevice().getId()).get();
+        BlockPeriod blockPeriodFound = blockPeriodRepository.findById(link.getBlock_period().getId()).get();
+
+        Link newlink = new Link(deviceFound,blockPeriodFound,link.getName(),link.getUrl());
+        Link savedLink = repo.save(newlink);
         return true;
     }
 }
