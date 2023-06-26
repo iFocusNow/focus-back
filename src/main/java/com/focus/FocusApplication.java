84f3df8a0c31e2d2cd5334eaf58fc0016a4b07fe
avi-2-avi <avicon95@protonmail.com>
2023-06-25 21:42:00 -0500

    feat: main function cleanup

--- src/main/java/com/focus/FocusApplication.java
+++ src/main/java/com/focus/FocusApplication.java
@@ -1,19 +1,10 @@
 package com.focus;
 
-import com.focus.model.Authority;
-import com.focus.model.AuthorityName;
-import com.focus.model.User;
-import com.focus.repository.AuthorityRepository;
-import com.focus.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.Date;
-import java.util.List;
 
 @SpringBootApplication
 @EntityScan("com.focus.model")
